781821995bb8ff136491e5efc3705ba56332ad4d
vladz <vladzholon@gmail.com>
2022-05-30 07:38:40 +0300

    Setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <el-main>
+    <MainHeader />
+    <router-view />
+  </el-main>
 </template>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
@@ -28,3 +24,9 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script>
+import MainHeader from '@/components/MainHeader'
+export default {
+  components: { MainHeader }
+}
+</script>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-header>
+    <el-row>
+      <h1>PlaybackArt</h1>
+    </el-row>
+  </el-header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
