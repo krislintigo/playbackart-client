3ecf349c59a8061628b92178cac223dc85179123
vladz <vladzholon@gmail.com>
2022-05-30 08:21:23 +0300

    Code working

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <el-main>
+  <el-main class="main">
     <MainHeader />
     <router-view />
   </el-main>
 </template>
 
 <style>
-#app {
+body, input, button, textarea, select {
+  box-sizing: border-box;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.main {
+  max-width: 1500px;
+  margin: 0 auto;
 }
 </style>
-<script>
+<script setup>
 import MainHeader from '@/components/MainHeader'
-export default {
-  components: { MainHeader }
-}
 </script>
