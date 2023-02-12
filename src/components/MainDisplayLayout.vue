b277fd196d35c57d6dac5d4dac571fd02faed098
vladz <vladzholon@gmail.com>
2023-02-12 15:15:41 +0300

    Pagination

--- src/components/MainDisplayLayout.vue
+++ src/components/MainDisplayLayout.vue
@@ -1,14 +1,14 @@
 <template>
   <el-collapse v-model="activeItems">
     <div v-for="(block, i) in displayedItems" :key="block.title">
-      <el-collapse-item v-if="block.items.length" :name="i" class="block-header">
+      <el-collapse-item v-if="block.paginated.length" :name="i" class="block-header">
         <template #title>
           <el-row>
             <h2 style="font-size: 18px">{{block.title}}</h2>
           </el-row>
         </template>
-        <el-table :data="block.items">
-          <el-table-column type="index" label="#" width="50" />
+        <el-table :data="block.paginated">
+          <el-table-column type="index" label="#" width="50" :index="indexHandler(block.pagination)" />
           <el-table-column sortable prop="name" label="Название" min-width="350">
             <template #default="scope">
               <el-popover placement="right" :width="420" trigger="hover" :show-after="300" :persistent="false">
@@ -57,9 +57,18 @@
             </template>
           </el-table-column>
         </el-table>
+        <el-pagination
+          v-model:current-page="block.pagination.page"
+          v-model:page-size="block.pagination.pageSize"
+          hide-on-single-page
+          layout="prev, pager, next"
+          :page-sizes="[20, 50, 100]"
+          :page-size="block.pagination.pageSize"
+          :total="block.total.length"
+        />
         <h4 style="margin: 5px 0">
           Общая продолжительность:
-          {{formatDuration(block.items.reduce((acc, cur) => acc + cur.time.count * cur.time.duration, 0)) || '-'}}
+          {{formatDuration(block.total.reduce((acc, cur) => acc + cur.time.count * cur.time.duration, 0)) || '-'}}
         </h4>
       </el-collapse-item>
     </div>
@@ -67,7 +76,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, defineProps, defineEmits, inject } from 'vue'
+import { computed, ref, defineProps, defineEmits, inject, reactive } from 'vue'
 import { Delete, EditPen } from '@element-plus/icons-vue'
 import ItemPreview from '@/components/ItemPreview.vue'
 import formatDuration from '@/utils/formatDuration'
@@ -91,33 +100,80 @@ const refetch = inject('refetch') as Function
 const activeItems = ref([0, 1, 2, 3, 4])
 
 const lookingItems = computed<Item[]>(() => props.items.filter(item => item.status === 'looking'))
+const lookingItemsPagination = reactive({
+  page: 1,
+  pageSize: 50
+})
 const plannedItems = computed<Item[]>(() => props.items.filter(item => item.status === 'planned'))
+const plannedItemsPagination = reactive({
+  page: 1,
+  pageSize: 50
+})
 const viewedItems = computed<Item[]>(() => props.items.filter(item => item.status === 'viewed'))
+const viewedItemsPagination = reactive({
+  page: 1,
+  pageSize: 50
+})
 const postponedItems = computed<Item[]>(() => props.items.filter(item => item.status === 'postponed'))
+const postponedItemsPagination = reactive({
+  page: 1,
+  pageSize: 50
+})
 const abandonedItems = computed<Item[]>(() => props.items.filter(item => item.status === 'abandoned'))
+const abandonedItemsPagination = reactive({
+  page: 1,
+  pageSize: 50
+})
 
 const displayedItems = computed(() => [
   {
     title: 'В ПРОЦЕССЕ',
-    items: lookingItems.value
+    paginated: lookingItems.value.slice(
+      (lookingItemsPagination.page - 1) * lookingItemsPagination.pageSize,
+      lookingItemsPagination.page * lookingItemsPagination.pageSize
+    ),
+    total: lookingItems.value,
+    pagination: lookingItemsPagination
   },
   {
     title: 'ЗАПЛАНИРОВАНО',
-    items: plannedItems.value
+    paginated: plannedItems.value.slice(
+      (plannedItemsPagination.page - 1) * plannedItemsPagination.pageSize,
+      plannedItemsPagination.page * plannedItemsPagination.pageSize
+    ),
+    total: plannedItems.value,
+    pagination: plannedItemsPagination
   },
   {
     title: 'ЗАВЕРШЕНО',
-    items: viewedItems.value
+    paginated: viewedItems.value.slice(
+      (viewedItemsPagination.page - 1) * viewedItemsPagination.pageSize,
+      viewedItemsPagination.page * viewedItemsPagination.pageSize
+    ),
+    total: viewedItems.value,
+    pagination: viewedItemsPagination
   },
   {
     title: 'ОТЛОЖЕНО',
-    items: postponedItems.value
+    paginated: postponedItems.value.slice(
+      (postponedItemsPagination.page - 1) * postponedItemsPagination.pageSize,
+      postponedItemsPagination.page * postponedItemsPagination.pageSize
+    ),
+    total: postponedItems.value,
+    pagination: postponedItemsPagination
   },
   {
     title: 'БРОШЕНО',
-    items: abandonedItems.value
+    paginated: abandonedItems.value.slice(
+      (abandonedItemsPagination.page - 1) * abandonedItemsPagination.pageSize,
+      abandonedItemsPagination.page * abandonedItemsPagination.pageSize
+    ),
+    total: abandonedItems.value,
+    pagination: abandonedItemsPagination
   }
 ])
+
+const indexHandler = (pagination: any) => (index: number) => (pagination.page - 1) * pagination.pageSize + index + 1
 
 const sortByRating = (a: Item, b: Item) => {
   if (!a.rating || !b.rating) return -1
